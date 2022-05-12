fa496748e97c1f78523ec2545e00e602e4b56b01
PauStack <paulopesruis@gmail.com>
2022-05-12 15:49:03 +0200

    Marketplace

---
+++ resources/views/inicio.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('content')
+    <div id="herosection">
+        <div class="heropart">
+            <img src="{{ asset('img/astronauta.png') }}"></img>
+        </div>
+        <div class="heropart">
+            <h1>Bienvenidos al Nuevo Mundo</h1>
+            <button>Ir al Mercado</button>
+        </div>
+        
+    </div>
+@endsection
